2c84bbb26c3c00bda11645cdbb3b9a60ea88412f
sahilinamdar-lang <sahilinamdar212@gmail.com>
2026-02-11 17:48:37 +0530

    solve errors  and code duplications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,8 +20,6 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
     return (
-       
-
         <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <AuthProvider>
                 <div className="min-h-screen bg-slate-50">
